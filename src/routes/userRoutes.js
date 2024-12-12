2584a70cb8199fdb596bb2d51152cacfdd0aa278
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-12-12 13:57:06 +0530

    Protect Routes from Unauthorized Users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware")
 
 const router = express.Router();
 
 // Only For Teachers
-router.get("/teacher", verifyToken, (req, res) => {
+router.get("/teacher", verifyToken, authorizeRoles("Teacher") ,(req, res) => {
     res.json({message: "Welcome Teacher !"});
 });
 
 // Both Teachers and Students 
-router.get("/user", verifyToken, (req, res) => {
+router.get("/user", verifyToken, authorizeRoles("Teacher", "Student"), (req, res) => {
     res.json({message: "Welcome All !"});
 });
 
 // Only For Students 
-router.get("/student", verifyToken, (req, res) => {
+router.get("/student", verifyToken, authorizeRoles("Student"), (req, res) => {
     res.json({message: "Welcome Student !"});
 });
 
